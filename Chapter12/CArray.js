7c20d0815be623ae8f1e1dad4bf8880a22e10a1e
Zhiqiang Zhang <zqzhang.cas@gmail.com>
2014-06-30 10:54:15 +0800

    Remove unreferenced variable from bubbleSort function

--- Chapter12/CArray.js
+++ Chapter12/CArray.js
@@ -60,9 +60,7 @@ function swap(arr, index1, index2) {
 // Example 12-3, the bubble sort
 // Example 12-5, adding a call to the toString() function of bubbleSort()
 function bubbleSort() {
-   var numElements = this.dataStore.length;
-   var temp;
-   for (var outer = numElements; outer >= 2; --outer) {
+   for (var outer = this.dataStore.length; outer >= 2; --outer) {
       for (var inner = 0; inner <= outer-1; ++inner) {
          if (this.dataStore[inner] > this.dataStore[inner+1]) {
             swap(this.dataStore, inner, inner+1);
